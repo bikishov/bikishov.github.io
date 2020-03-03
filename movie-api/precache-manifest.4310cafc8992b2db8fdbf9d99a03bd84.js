cc28f1869cd83b819e652af52a0858f3bc71f9c4
bikishov <bikishovd@gmail.com>
2020-03-03 22:54:36 +0300

    change react

---
+++ movie-api/precache-manifest.4310cafc8992b2db8fdbf9d99a03bd84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8181c20dc9f7a4229d63a3fa4bd81e39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bac470d101e5037a8c15",
+    "url": "/static/css/main.da7956fc.chunk.css"
+  },
+  {
+    "revision": "e225652a7e9cd5b8eb72",
+    "url": "/static/js/2.5e235a40.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.5e235a40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac470d101e5037a8c15",
+    "url": "/static/js/main.2e088e95.chunk.js"
+  },
+  {
+    "revision": "00573b00428d1cbe9945",
+    "url": "/static/js/runtime-main.47cdcd70.js"
+  }
+]);
